96d9289b474d1b82106629e8c4c7f0ed87a432e0
Dzmitry Struneuski <isidbrest@gmail.com>
2024-05-15 07:31:21 +0000

    modified:   reimage_sertis.sh

--- reimage_sertis.sh
+++ reimage_sertis.sh
@@ -4,4 +4,4 @@ sudo apt-get update -y && sudo apt-get dist-upgrade -y && sudo apt autoremove
 sudo docker stop sertis && sudo docker rm sertis
 sudo docker image rm sertis_by
 sudo docker build --no-cache -t sertis_by .
-sudo docker run -d -p 80:80 --name sertis sertis_by
+sudo docker run -d -p 80:80 --name sertis --restart always sertis_by
